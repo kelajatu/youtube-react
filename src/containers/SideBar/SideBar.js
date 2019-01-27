67edd2682ce5b52c64909e2bc075785ff3da2c1a
your <jurgenkela@gmail.com>
2019-01-27 18:36:38 -0500

    sidebarfooter completed - MyTUbe sidebar completed finished

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -3,6 +3,8 @@ import { SideBarItem } from "./SideBarItem/SideBarItem";
 import { Menu, Divider } from "semantic-ui-react";
 import "./SideBar.scss";
 import { SideBarHeader } from "./SideBarHeader/SideBarHeader";
+import { Subscriptions } from "./Subscriptions/Subscriptions";
+import { SideBarFooter } from "./SideBarFooter/SideBarFooter";
 
 export class SideBar extends React.Component {
   render() {
@@ -17,6 +19,7 @@ export class SideBar extends React.Component {
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
         <Divider />
+        <Subscriptions />
         <SideBarHeader title="More from Youtube" />
         <SideBarItem label="Movies and Shows" icon="film" />
         <Divider />
@@ -24,6 +27,7 @@ export class SideBar extends React.Component {
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
         <Divider />
+        <SideBarFooter />
       </Menu>
     );
   }
