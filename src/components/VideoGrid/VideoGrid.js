fae8d74cb43d603baece184528b154e878f2dff6
your <jurgenkela@gmail.com>
2019-01-31 10:51:32 -0500

    VideoGridheader.scss completed and Home.js file updated

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./VideoGrid.scss";
-// import { VideoGridHeader } from "./VideoGridHeader/VideoGridHeader";
+import { VideoGridHeader } from "./VideoGridHeader/VideoGridHeader";
 import { Divider } from "semantic-ui-react";
 import { VideoPreview } from "../VideoPreview/VideoPreview";
 
@@ -8,7 +8,7 @@ export function VideoGrid(props) {
   const divider = props.hideDivider ? null : <Divider />;
   return (
     <React.Fragment>
-      <h4>Trending</h4>
+      <VideoGridHeader title="Trending" />
       <div className="video-grid">
         <VideoPreview />
         <VideoPreview />
