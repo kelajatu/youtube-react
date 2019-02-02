afafcf7cecc446719d4ca80c17d6b4ded70a0ff3
your <jurgenkela@gmail.com>
2019-02-02 17:52:07 -0500

    created RelatedVideos component and fixed routing of related videos routing

--- src/components/RelatedVideos/RelatedVideos.js
+++ src/components/RelatedVideos/RelatedVideos.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { VideoPreview } from "../VideoPreview/VideoPreview";
 import "./RelatedVideos.scss";
+import { NextUpVideo } from "../NextUpVideo/NextUpVideo";
 
 export function RelatedVideos(props) {
   return (
     <div className="related-videos">
+      <NextUpVideo />
       <VideoPreview horizontal={true} />
       <VideoPreview horizontal={true} />
       <VideoPreview horizontal={true} />
