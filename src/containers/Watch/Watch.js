df5e342ce61b2688ba148d82f2f675570bb688b7
your <jurgenkela@gmail.com>
2019-02-02 18:42:33 -0500

    Watch container updated to include Video component and spacing styles

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
+import "./Watch.scss";
 import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
+import { Video } from "../../components/Video/Video";
 
 export class Watch extends React.Component {
   render() {
-    return <RelatedVideos />;
+    return (
+      <div className="watch-grid">
+        <Video className="video" id="-7fuHEEmEjs" />
+        <div
+          className="metadata"
+          style={{ width: "100%", height: "100px", background: "#F91112" }}
+        >
+          Metadata
+        </div>
+        <div
+          className="video-info-box"
+          style={{ width: "100%", height: "100px", background: "#BD10E0" }}
+        >
+          Video Info box
+        </div>
+        <div
+          className="comments"
+          style={{ width: "100%", height: "100px", background: "#9013FE" }}
+        >
+          comments
+        </div>
+        <RelatedVideos className="relatedVideos" />
+      </div>
+    );
   }
 }
