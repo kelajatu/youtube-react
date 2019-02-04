b374418d38fbc1b585bf7d116eef13fb760b8cc2
your <jurgenkela@gmail.com>
2019-02-04 15:58:54 -0500

    added js and css structure of VideoInfoBox

---
+++ src/components/VideoInfoBox/VideoInfoBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./VideoInfoBox.scss";
+import { Image, Button } from "semantic-ui-react";
+
+export class VideoInfoBox extends React.Component {
+  render() {
+    return <div className="video-info-box">{todo}</div>;
+  }
+}
