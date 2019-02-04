da13833f7af199ce56c41d0d4912d690e0b5d77e
your <jurgenkela@gmail.com>
2019-02-04 14:04:01 -0500

    VideoMetadata.unit.test completed and passing

--- src/components/VideoMetadata/__tests__/VideoMetadata.unit.test.js
+++ src/components/VideoMetadata/__tests__/VideoMetadata.unit.test.js
@@ -1,3 +1,14 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { VideoMetadata } from "../VideoMetadata";
+
+describe("VideoMetadata", () => {
+  it("renders without props", () => {
+    const wrapper = shallow(<VideoMetadata />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("renders with viewcoubt", () => {
+    const wrapper = shallow(<VideoMetadata viewCount={1000} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
