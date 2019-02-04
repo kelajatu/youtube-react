c1aa0a2495b7b269475b254a4621ef010b833131
your <jurgenkela@gmail.com>
2019-02-04 13:29:28 -0500

    created Rating

--- src/components/VideoMetadata/VideoMetadata.js
+++ src/components/VideoMetadata/VideoMetadata.js
@@ -5,10 +5,11 @@ import "./VideoMetadata.scss";
 export function VideoMetadata(props) {
   const viewCount = Number(props.viewCount).toLocaleString() || "";
   return (
-    <div className="video-medtadata">
+    <div className="video-metadata">
       <h3>Video title</h3>
       <div className="video-stats">
         <span>{viewCount} views</span>
+        <div>{/*TODO*/}</div>
       </div>
       <Divider />
     </div>
