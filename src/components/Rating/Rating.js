526c49fc19989d02dfe6b8d1b9df433dcbfb7876
your <jurgenkela@gmail.com>
2019-02-04 13:53:01 -0500

    added progress bar and fixed rendering bug

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./Rating.scss";
 import { Icon, Progress } from "semantic-ui-react";
 
-export function Rating() {
+export function Rating(props) {
   let progress = null;
-
   if (props.likeCount && props.dislikeCount) {
     const percent =
       100 * (props.likeCount / (props.likeCount + props.dislikeCount));
     progress = <Progress className="progress" percent={percent} size="tiny" />;
   }
-
   return (
     <div className="rating">
       <div className="thumbs-up">
