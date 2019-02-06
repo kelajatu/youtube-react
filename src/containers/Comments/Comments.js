081775f258b96d48613b107c2fcb32db83d865d2
your <jurgenkela@gmail.com>
2019-02-06 09:50:59 -0500

    wired up comment and addcomment components to comments container

--- src/containers/Comments/Comments.js
+++ src/containers/Comments/Comments.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { CommentsHeader } from "./CommentsHeader/CommentsHeader";
+import { AddComment } from "./AddComment/AddComment";
+import { Comment } from "./Comment/Comment";
 
 export class Comments extends React.Component {
   render() {
     return (
       <div>
         <CommentsHeader amoountComments={this.props.amoountComments} />
+        <AddComment />
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     );
   }
