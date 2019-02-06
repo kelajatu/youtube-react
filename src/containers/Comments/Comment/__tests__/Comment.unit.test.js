2693254c21d8585db414a6a6b43a4abbdfc5bfbf
your <jurgenkela@gmail.com>
2019-02-06 11:20:16 -0500

    AddComment test created and passing

--- src/containers/Comments/Comment/__tests__/Comment.unit.test.js
+++ src/containers/Comments/Comment/__tests__/Comment.unit.test.js
@@ -1,3 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { Comment } from "../Comment";
+
+describe("Comment", () => {
+  it("renders Comment", () => {
+    const wrapper = shallow(<Comment />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
