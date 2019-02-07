81985da98af63ca28532659852875727e27a8b69
your <jurgenkela@gmail.com>
2019-02-06 20:25:39 -0500

    created configureStore

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+
+export function configureStore() {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+  sagaMiddleware.run(rootSaga);
+}
