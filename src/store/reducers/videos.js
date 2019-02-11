a6a80524d5b6018605bedc6bbe064ae2c5536c8a
your <jurgenkela@gmail.com>
2019-02-11 18:40:36 -0500

    added Reducefetchmostpopularvideos for reducer

--- src/store/reducers/videos.js
+++ src/store/reducers/videos.js
@@ -5,6 +5,7 @@ const initialState = {
   byId: {},
   mostPopular: {}
 };
+
 export default function videos(state = initialState, action) {
   switch (action.type) {
     case MOST_POPULAR[SUCCESS]:
@@ -12,4 +13,28 @@ export default function videos(state = initialState, action) {
     default:
       return state;
   }
+}
+
+function reduceFetchMostPopularVideos(response, prevState) {
+  const videoMap = response.items.reduce((accumulator, video) => {
+    accumulator[video.id] = video;
+    return accumulator;
+  }, {});
+
+  let items = Object.keys(videoMap);
+  if (response.hasOwnProperty("prevPageToken") && prevState.mostPopular) {
+    items = [...prevState.mostPopular.items, ...items];
+  }
+
+  const mostPopular = {
+    totalResults: response.pageInfo.totalResults,
+    nextPageToken: response.nextPageToken,
+    items
+  };
+
+  return {
+    ...prevState,
+    mostPopular,
+    byId: { ...prevState.byId, ...videoMap }
+  };
 }
