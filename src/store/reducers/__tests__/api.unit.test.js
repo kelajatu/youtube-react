7aa461a1344b1df16c452e10af1d24c7bfbf5425
your <jurgenkela@gmail.com>
2019-02-12 12:00:17 -0500

    api tests completed

--- src/store/reducers/__tests__/api.unit.test.js
+++ src/store/reducers/__tests__/api.unit.test.js
@@ -6,11 +6,30 @@ const initialState = {
 };
 
 describe("api Reducer", () => {
-  it("tests unised type with default initial state", () => {
+  it("tests unused type with default initial state", () => {
     const unusedActionType = "unused-action-type";
     const expectedEndState = { ...initialState };
     expect(apiReducer(undefined, { type: unusedActionType })).toEqual(
       expectedEndState
+    );
+  });
+
+  it("test api reducer with YOUTUBE_LIBRARY_LOADED action", () => {
+    const startState = { ...initialState };
+    const expectedEndState = {
+      libraryLoaded: true
+    };
+    expect(apiReducer(startState, { type: YOUTUBE_LIBRARY_LOADED })).toEqual(
+      expectedEndState
+    );
+  });
+
+  it("test api reducer for idempotence with YOUTUBELIBRARYLOADED action", () => {
+    const startState = {
+      libraryLoaded: true
+    };
+    expect(apiReducer(startState, { type: YOUTUBE_LIBRARY_LOADED })).toEqual(
+      startState
     );
   });
 });
