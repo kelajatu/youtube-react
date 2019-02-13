faaa1c1897d9f05ce6bf7832cee6457c42124095
your <jurgenkela@gmail.com>
2019-02-13 16:31:03 -0500

    updated video grid to dynamically render

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -1,28 +1,22 @@
 import React from "react";
+import "./VideoGrid.scss";
 import { VideoGridHeader } from "./VideoGridHeader/VideoGridHeader";
 import { Divider } from "semantic-ui-react";
 import { VideoPreview } from "../VideoPreview/VideoPreview";
-import "./VideoGrid.scss";
 
 export function VideoGrid(props) {
+  if (!props.videos || !props.videos.length) {
+    return <div />;
+  }
+  const gridItems = props.videos.map(video => {
+    return <VideoPreview video={video} key={video.id} />;
+  });
+
   const divider = props.hideDivider ? null : <Divider />;
   return (
     <React.Fragment>
-      <VideoGridHeader title="Trending" />
-      <div className="video-grid">
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-        <VideoPreview />
-      </div>
+      <VideoGridHeader title={props.title} />
+      <div className="video-grid">{gridItems}</div>
       {divider}
     </React.Fragment>
   );
