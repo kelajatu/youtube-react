8f6ef34039562556368ab2538fafb83505621a4c
your <jurgenkela@gmail.com>
2019-02-12 21:05:09 -0500

    reducer test written, test not passing yet

--- src/store/reducers/__tests__/videos.unit.test.js
+++ src/store/reducers/__tests__/videos.unit.test.js
@@ -3,7 +3,6 @@ import { MOST_POPULAR } from "../../actions/video";
 import { SUCCESS } from "../../actions";
 import mostPopularResponse from "./responses/MOST_POPULAR_SUCCESS";
 import mostPopularSuccessState from "./states/MOST_POPULAR_SUCCESS";
-import videos from "../videos";
 
 const initialState = {
   byId: {},
@@ -11,10 +10,23 @@ const initialState = {
 };
 
 describe("videos reducer", () => {
-  it("test undefined action type initial state with videos reducer", () => {
+  test("test undefined action type and initial state with videos reducer", () => {
     const expectedEndState = { ...initialState };
     expect(videosReducer(undefined, { type: "some-unused-type" })).toEqual(
       expectedEndState
     );
+  });
+
+  test("test MOST_POPULAR_SUCCESS action in video reducer", () => {
+    const startState = { ...initialState };
+    const action = {
+      type: MOST_POPULAR[SUCCESS],
+      response: mostPopularResponse
+    };
+    const expectedEndState = {
+      ...startState,
+      ...mostPopularSuccessState
+    };
+    expect(videosReducer(startState, action)).toEqual(expectedEndState);
   });
 });
