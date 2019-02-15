1623cfaa7dee350ef6efffdc18049fbd01ad4a8e
your <jurgenkela@gmail.com>
2019-02-15 17:38:49 -0500

    formatted VideoPreview for times

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -5,6 +5,8 @@ import "./VideoPreview.scss";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import { getShortNumberString } from "../../services/number/number-format";
+import { getVideoDurationString } from "../../services/date/date-format";
+import { Link } from "react-router-dom";
 
 TimeAgo.locale(en);
 const timeAgo = new TimeAgo("en-US");
@@ -16,6 +18,10 @@ export class VideoPreview extends React.Component {
       return <div />;
     }
 
+    const duration = video.contentDetails
+      ? video.contentDetails.duration
+      : null;
+    const videoDuration = getVideoDurationString(duration);
     const viewAndTimeString = VideoPreview.getFormattedViewAndTime(video);
     const horizontal = this.props.horizontal ? "horizontal" : null;
 
@@ -24,7 +30,7 @@ export class VideoPreview extends React.Component {
         <div className="image-container">
           <Image src={video.snippet.thumbnails.medium.url} />
           <div className="time-label">
-            <span>{video.contentDetails.duration}</span>
+            <span>{videoDuration}</span>
           </div>
         </div>
 
