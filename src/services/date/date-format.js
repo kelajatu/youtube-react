259d03251dec7378581054f8dadab2eb18bdbbf6
your <jurgenkela@gmail.com>
2019-02-15 17:33:08 -0500

    parse ISO 8601, copied from github

--- src/services/date/date-format.js
+++ src/services/date/date-format.js
@@ -1,1 +1,15 @@
-export function parseISO8601TimePattern(durationString) {}
+const objMap = ["years", "months", "days", "hours", "minutes", "seconds"];
+const numbers = "\\d+(?:[\\.,]\\d{0,3})?";
+const datePattern = `(${numbers}Y)?(${numbers}M)?(${numbers}D)?`;
+const timePattern = `T(${numbers}H)?(${numbers}M)?(${numbers}S)?`;
+const pattern = new RegExp(`P(?:${datePattern}(?:${timePattern})?)`);
+
+export function parseISO8601TimePattern(durationString) {
+  return durationString
+    .match(pattern)
+    .slice(1)
+    .reduce((prev, next, idx) => {
+      prev[objMap[idx]] = parseFloat(next) || 0;
+      return prev;
+    }, {});
+}
