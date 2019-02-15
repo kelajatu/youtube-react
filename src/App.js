b70a9c01422bc60216230e4babe8ae42606942a3
your <jurgenkela@gmail.com>
2019-02-15 16:37:19 -0500

    time ago dependancy added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
 import Home from "./containers/Home/Home";
 import { AppLayout } from "./components/AppLayout/AppLayout";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { Watch } from "./containers/Watch/Watch";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { youtubeLibraryLoaded } from "./store/actions/api";
+import Trending from "./containers/Trending/Trending";
+import Search from "./containers/Search/Search";
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
+const API_KEY = "<enter your key here>";
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
         <Switch>
-          <Route path="/watch" component={Watch} />
+          <Route path="/feed/trending" component={Trending} />
+          <Route
+            path="/results"
+            render={() => <Search key={this.props.location.key} />}
+          />
+          <Route
+            path="/watch"
+            render={() => <Watch key={this.props.location.key} />}
+          />
           <Route path="/" component={Home} />
         </Switch>
       </AppLayout>
     );
   }
-
   componentDidMount() {
     this.loadYoutubeApi();
   }
@@ -46,7 +55,9 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({ youtubeLibraryLoaded }, dispatch);
 }
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(App)
+);
