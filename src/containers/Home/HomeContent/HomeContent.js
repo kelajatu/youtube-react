0533018b1f077d4c48e3b4bbfd25ee8d72e356b9
your <jurgenkela@gmail.com>
2019-02-20 18:08:54 -0500

    implemented video grid, sections showing

--- src/containers/Home/HomeContent/HomeContent.js
+++ src/containers/Home/HomeContent/HomeContent.js
@@ -12,10 +12,13 @@ const AMOUNT_TRENDING_VIDEOS = 12;
 class HomeContent extends React.Component {
   render() {
     const trendingVideos = this.getTrendingVideos();
+    const categoryGrids = this.getVideoGridsForCategories();
+
     return (
       <div className="home-content">
         <div className="responsive-video-grid-container">
           <VideoGrid title="Trending" videos={trendingVideos} />
+          {categoryGrids}
         </div>
       </div>
     );
@@ -23,6 +26,22 @@ class HomeContent extends React.Component {
 
   getTrendingVideos() {
     return this.props.mostPopularVideos.slice(0, AMOUNT_TRENDING_VIDEOS);
+  }
+
+  getVideoGridsForCategories() {
+    const categoryTitles = Object.keys(this.props.videosByCategory || {});
+    return categoryTitles.map((categoryTitle, index) => {
+      const videos = this.props.videosByCategory[categoryTitle];
+      const hideDivider = index === categoryTitles.length - 1;
+      return (
+        <VideoGrid
+          title={categoryTitle}
+          videos={videos}
+          key={categoryTitle}
+          hideDivider={hideDivider}
+        />
+      );
+    });
   }
 }
 
