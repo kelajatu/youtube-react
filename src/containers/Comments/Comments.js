5eb18db315a9a9d05f35a37ededfa2acf1019e9d
your <jurgenkela@gmail.com>
2019-03-04 13:01:10 -0500

    wired up to display comments

--- src/containers/Comments/Comments.js
+++ src/containers/Comments/Comments.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { CommentsHeader } from "./CommentsHeader/CommentsHeader";
-import { AddComment } from "./AddComment/AddComment";
 import { Comment } from "./Comment/Comment";
+import { AddComment } from "./AddComment/AddComment";
 
 export class Comments extends React.Component {
   render() {
+    if (!this.props.comments) {
+      return <div />;
+    }
+
+    const comments = this.props.comments.map(comment => {
+      return <Comment comment={comment} key={comment.id} />;
+    });
     return (
       <div>
         <CommentsHeader amoountComments={this.props.amoountComments} />
-        <AddComment />
-        <Comment />
-        <Comment />
-        <Comment />
+        <AddComment key="add-comment" />
+        {comments}
       </div>
     );
   }
