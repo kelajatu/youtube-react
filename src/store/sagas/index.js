387e412d1b7080e0bddde8355b09d5cf38392519
your <jurgenkela@gmail.com>
2019-03-04 18:34:00 -0500

    plugged watcher saga into root saga reducer

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -5,7 +5,7 @@ import {
   watchVideoCategories
 } from "./video";
 import { watchWatchDetails } from "./watch";
-// import { watchCommentThread } from "./comment";
+import { watchCommentThread } from "./comment";
 // import { watchSearchForVideos } from "./search";
 
 export default function*() {
@@ -13,8 +13,8 @@ export default function*() {
     fork(watchMostPopularVideos),
     fork(watchVideoCategories),
     fork(watchMostPopularVideosByCategory),
-    fork(watchWatchDetails)
-    // fork(watchCommentThread),
+    fork(watchWatchDetails),
+    fork(watchCommentThread)
     // fork(watchSearchForVideos)
   ]);
 }
